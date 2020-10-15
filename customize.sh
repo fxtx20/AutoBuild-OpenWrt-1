6a52faf1ac22cdc28b2236d5d163f65a74645d29
fxtx2020 <64974867+fxtx2020@users.noreply.github.com>
2020-10-15 18:09:09 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,4 +7,4 @@
 #=================================================
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.5.10/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.10/g' openwrt/package/base-files/files/bin/config_generate
